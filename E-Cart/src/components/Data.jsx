0b22bf00f668f11af712a541179261224495057e
Junu <hgit2025@gmail.com>
2025-01-19 01:59:32 +0600

    Project

--- E-Cart/src/components/Data.jsx
+++ E-Cart/src/components/Data.jsx
@@ -6,20 +6,20 @@ export const items = [
     imgSrc: "https://m.media-amazon.com/images/I/71xb2xkN5qL._SL1500_.jpg",
     amazonLink: "https://amzn.to/3PuckZp",
     description: "Apple iPhone 14 (128 GB) - Blue",
-    price: "89999",
+    price: "90999",
   },
   {
     id: 10,
-    category: "Android",
+    category: "mobiles",
     title: "Samsung Galaxy S24 Ultra 5G",
     imgSrc:
-      "https://brotherselectronicsbd.com/image/cache/catalog/demo/product/Samsung/S24%20Ultra/Samsung-Galaxy-S24-ultra-Titanium-Gray-800x800.jpg",
+      "https://d61s2hjse0ytn.cloudfront.net/color/864/Titanium_Black_S24_Ultra.webp",
     amazonLink:
       "https://www.amazon.in/Samsung-Galaxy-Ultra-Green-Storage/dp/B0BT9CXXXX/ref=sr_1_1_sspa?dib=eyJ2IjoiMSJ9.O3xB_kiiFUsvf5zmsm5j4g8vzEK17lmDvw9dtn0_XEsGhZ-yQGnazJ796J6Z9Q7qN6tZCwLFzXJj0yj_UHkeAEY8KYp8I0qKTcQN8jWWP_vCy8j1Cw1aAWSeOsbVAs-eMZqb5XqhhWpo_ZR5SMNETCYxysAE5hWuBkLaR6ZoKgkyOYF57xNmdVChovkjqygyqdJUxeDcGkQdtJtnOmln6yhVX9A3ZHDlQe0PbgotGfo.PzUO-omJZuGcZEZonh960HIAaG_JkQaxDG4TEt37Bak&dib_tag=se&keywords=S24&qid=1737128302&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1",
     description: "Samsung Galaxy S24 Ultra 5G (Green, 12GB RAM, 256GB Storage)",
     price: "80999",
   },
-  
+
   {
     id: 2,
     category: "mobiles",
@@ -28,7 +28,7 @@ export const items = [
     description: "Apple iPhone 13 (64GB) - Blue",
     price: "79999",
   },
-   {
+  {
     id: 3,
     category: "mobiles",
     title: "Apple iPhone 13",
@@ -41,21 +41,21 @@ export const items = [
     id: 5,
     category: "laptops",
     title: "Macbook M1",
-    imgSrc: "https://m.media-amazon.com/images/I/71TPda7cwUL._SL1500_.jpg",
-    amazonLink: "https://amzn.to/3EzeQaC",
+    imgSrc: "https://m.media-amazon.com/images/I/71jG+e7roXL._SY450_.jpg",
+    amazonLink: "https://www.amazon.in/Apple-MacBook-Chip-13-inch-256GB/dp/B08N5W4NNB/ref=sr_1_1_sspa?crid=28HMENG7D73KW&dib=eyJ2IjoiMSJ9.3T8fjFyvGONWBTVI0Dpqdl4PyRvlcwLvFvVLPGIvAtocQYTEkBpS_Ho8GaQzaJZOrIoCutg6ndqa0zJj9bAYyd-XEe-GbCL9MklFCulr-JrA0fUrwhQdoaFqVEXNZOq4e_xg2myZdKq6Gc-gYsm8uZ-GWeBijHDrguUUHGoJh71kmn9yJfVD9QT5HaeE_Bw0yM0P5pWDUKViB7wo66XaQA4OQX5cazA0tZESs9ew0nQ.UuDp97WkdhwIIaOuXOCYv2QuYYk2hapWynnjKRoGelM&dib_tag=se&keywords=macbook+m1&qid=1737224223&sprefix=mac%2Caps%2C282&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1",
     description: "Macbook M1 13 inch 8GB RAM 256GB SSD",
-    price: "49999",
+    price: "69999",
   },
   {
     id: 7,
     category: "tablets",
     title: "Xiaomi Pad 6",
-    imgSrc: "https://brotherselectronicsbd.com/image/cache/catalog/demo/Accessories/Xiaomi/Pad6%20Key/pad%206%20keyboard-600x600.jpg",
+    imgSrc: "https://d61s2hjse0ytn.cloudfront.net/color/854/xiaomi_pad_6_black.webp",
     amazonLink: "https://amzn.to/3ZeNRdY",
     description: "Xiaomi Pad 6| Qualcomm Snapdragon 870| ",
     price: "29999",
   },
-  
+
   {
     id: 6,
     category: "tablets",
@@ -66,15 +66,15 @@ export const items = [
       "Lenovo Tab P12 Pro AMOLED (12.6 inch (32 cm), 8 GB, 256 GB, Wi-fi Only), Storm Grey with Precision Pen 3",
     price: "19999",
   },
- 
+
   {
     id: 4,
     category: "laptops",
     title: "Lenovo IdeaPad Slim 5 ",
-    imgSrc: "https://m.media-amazon.com/images/I/71TPda7cwUL._SL1500_.jpg",
-    amazonLink: "https://amzn.to/44JsK4r",
+    imgSrc: "https://m.media-amazon.com/images/I/612ealjuNbL._SX679_.jpg",
+    amazonLink: "https://www.amazon.in/Lenovo-IdeaPad-300Nits-BacklitKB-83BF0043IN/dp/B0CNK1VTZW/ref=sr_1_3?crid=2Z4W205NAO7UK&dib=eyJ2IjoiMSJ9.CpMv13kbBmMoGPROYuqkDiTr8Qn90GHnbPPqsgV5CHqSFRAOXu9zCIVdR3oqQVhJWXAbcUV02o_n61VDCcnoMVeqIuzJd322Nzuru5JGF9Ab0JvNo4gj91e2DDDLDwd8CiHLRzsSAllJoYj27XdgRiNMmSsbJDDtVvo54cBHSuaU9sBFXcjiI4PiEgcX7erhdGiu40sdZoWjOV_rhN1jWtu5uEaQ0nIYWpcFp8X37N-Rh9z2PE3K-Je8jXQO2t-PKZ56n9F6tokXcgFOHg6oNwERdGamZmXQ4OZilCzjko0x0qmsTai-vPhN2iF4_DzYX0PSt6yeXgayomS82vOUCk1LUbET-VvBq2Nqi83qeBc.Zfc36iuy7JI5_3_zfPlPNyNKhlEMRX3HDjxa-S99MUg&dib_tag=se&keywords=Lenovo+IdeaPad+Slim+5&nsdOptOutParam=true&qid=1737207328&s=electronics&sprefix=lenovo+ideapad+slim+5%2Celectronics%2C394&sr=1-3",
     description: 'Lenovo IdeaPad Slim 5 Intel Core i5 12450H 14" (35.5cm) ',
-    price: "29999",
+    price: "59999",
   },
   {
     id: 9,
@@ -96,4 +96,164 @@ export const items = [
       "Apple MacBook Air Laptop M1 chip, 13.3-inch/33.74 cm Retina Display, ",
     price: "89999",
   },
+
+
+  {
+    id: 11,
+    category: "mobiles",
+    title: "Samsung Galaxy S23 Ultra 5G",
+    imgSrc: "https://gadgetbd.com/wp-content/uploads/2023/02/Website-Main-Image-copy.jpg",
+    amazonLink: "https://www.amazon.in/gp/aw/d/B0BT9CXXXX/?_encoding=UTF8&pd_rd_plhdr=t&aaxitk=b0d6271fee0f897acd049c62e02ec2e3&hsa_cr_id=0&qid=1737204754&sr=1-1-e0fa1fdd-d857-4087-adda-5bd576b25987&ref_=sbx_be_s_sparkle_lsi4d_asin_0_img&pd_rd_w=9inmz&content-id=amzn1.sym.df9fe057-524b-4172-ac34-9a1b3c4e647d%3Aamzn1.sym.df9fe057-524b-4172-ac34-9a1b3c4e647d&pf_rd_p=df9fe057-524b-4172-ac34-9a1b3c4e647d&pf_rd_r=VZHYFP25D4BZPA5MAQBB&pd_rd_wg=cQEcT&pd_rd_r=e79d5810-41c5-4aa7-92fc-c370833fb998",
+    description: "Samsung Galaxy S23 Ultra 5G AI Smartphone (Green, 12GB, 256GB Storage)",
+    price: "80999",
+  },
+
+  {
+    id: 12,
+    category: "mobiles",
+    title: "Samsung Galaxy S22 Ultra 5G",
+    imgSrc:
+      "https://m.media-amazon.com/images/I/810b1GFtl9L._SX679_.jpg",
+    amazonLink:
+      "https://www.amazon.in/Samsung-Burgundy-Storage-Additional-Exchange/dp/B09SH9D5PT/ref=sr_1_3?crid=2IZBPJ7RV8K3B&dib=eyJ2IjoiMSJ9.lYg7-vZm01PIQqQHMPyNlEFECol1TBrWHjfZlBsfjuLgw0NIandlfbCVAzJTe5Hgrz38j8uJWgb1Ecaj5qkeUbfiPLejvNH7ayyV3eePdZJULMdN0IbFnwAyJugpOjENtpyD9xINw8bnpb6kUgA86nmHhzjhlz01ZUcnQbGLpaZ1slZ5kLi3iiud91wE50roIbSYMJ8I9k8HnV6OXpk9cadLF_J1TWe5Ih7YZOlPtjiref1FORoiEUGpVi0Iq17EqhuCzecRAG-U96TRLh9cVduNpO_NK-v9Ky8wwg_YTqn4lKS_XdQqq7po0cHpqIrYCheW7VOlJf4hwkd5W1OnYEwTfC6VitbbGobfAvAcQDo.4SJJ_AiAwO65QMgKd9GoAtXwemhvZuR-g7Fe90-_Pgs&dib_tag=se&keywords=s22%2Bultra&qid=1737206411&s=electronics&sprefix=s22%2Celectronics%2C217&sr=1-3&th=1",
+    description: "Samsung Galaxy S22 Ultra 5G (Burgundy, 12GB, 512GB Storage)",
+    price: "70999",
+  },
+
+  {
+    id: 13,
+    category: "mobiles",
+    title: "Samsung Galaxy S23 FE 5G",
+    imgSrc:
+      "https://m.media-amazon.com/images/I/61-E0V81kBL._SX679_.jpg",
+    amazonLink:
+      "https://www.amazon.in/SAMSUNG-Galaxy-S23-Graphite-Storage/dp/B0CJXQX3MB/ref=sr_1_10?crid=D37WXO8IGG3Q&dib=eyJ2IjoiMSJ9.Tp-_bp4Q0mn-QVZ-Qk-_3r1DwJWTYFZZFAKSJTriYBi8yPxEtlNfk8-MV8pTXFIlK_KQt7e72LyOOvWpcWyByesH2pHNBH8DYUOJMb849Sd8U6QZQ81TJ0a76xda5ed-9x8TCqzDLLNPXuem45FdycTFHbaroYtPvMOI43i-CTaGWs9-KX5dxPOxA-zxkZ3wUtUxmCImykeS5uCHiZcyu7wGaZ7v0EELIqklLkCrU4hOJ5VgYh2Vvc0GMjsvAzfF_A64WT45ejagzYYaieIgjL8Hvtgd6O6s05hqOg8YpwFW7uJuiB6D4a53hIfFPqAEQ_5Wm1JJ5UZrbmOp4USFkQ-hQPC2UQojccquhinfpWc.pcqXR8iORB_0VF8hl4HDhFrQ-Ej1ddsYClAnwJR4wJ4&dib_tag=se&keywords=s21%2Bultra&nsdOptOutParam=true&qid=1737206836&s=electronics&sprefix=s21%2Bultra%2Celectronics%2C443&sr=1-10&th=1",
+    description: "Samsung Galaxy S23 FE 5G (Graphite 128 GB Storage) (8 GB RAM)",
+    price: "39999",
+  },
+
+  {
+    id: 14,
+    category: "tablets",
+    title: "Samsung Galaxy Tab A9+",
+    imgSrc: "https://m.media-amazon.com/images/I/61afkty1foL._SX522_.jpg",
+    amazonLink: "https://www.amazon.in/Samsung-Galaxy-Tab-A9-Expandable/dp/B0CJ38X1BQ/ref=sr_1_1_sspa?crid=3GCJMO6XXQWK4&dib=eyJ2IjoiMSJ9.CyT-gugaUUk3nWx09IRpf2YWZwNnx5YEvMPGkP_-hanXpB1gx_dcinE_0nKcaW69b6OXihSwsneNcsSsAa_HYaNg4Iswx8iV4JefO3YnZrozFL0TDpWbXxW5W_Dl-abad_4_CzPJ5faH3MrnOwAPxMRm4Lc1k0CvBvcIkYfGxo8mdMlMyvtLeFdS2hrLjqvoEMhhDDkDttnBGeai8DfWjPZHavx8z_wNQbJVYG-SK_KzejHfMVVEKNv3tDl0aX9yD8AytUT5lYaHybGBOUP6JeN0Io7uxTYgekzmLyAQdPc.W1zVoASaMtLRyCaEWVCq2PQUZ3Lti_4sxsFtTMO_AIw&dib_tag=se&keywords=Samsung+Galaxy+Tab+A8&nsdOptOutParam=true&qid=1737207472&s=electronics&sprefix=samsung+galaxy+tab+a8%2Celectronics%2C578&sr=1-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1",
+    description:
+      "Samsung Galaxy Tab A9+ 27.94 cm (11.0 inch) Display, RAM 8 GB, ROM 128 GB Expandable, Wi-Fi Tablet, Graphite",
+    price: "17999",
+  },
+
+  {
+    id: 15,
+    category: "tablets",
+    title: "HONOR Pad X8A",
+    imgSrc: "https://m.media-amazon.com/images/I/6104lCOYMJL._SX522_.jpg",
+    amazonLink: "https://www.amazon.in/HONOR-Flip-Cover-Expandable-Speakers-Snapdragon/dp/B0DF2TBQBH/ref=sxin_15_pa_sp_search_thematic_sspa?content-id=amzn1.sym.cd33fa60-47cd-49f3-80b8-24b1f0cbef97%3Aamzn1.sym.cd33fa60-47cd-49f3-80b8-24b1f0cbef97&crid=3GCJMO6XXQWK4&cv_ct_cx=Samsung+Galaxy+Tab+A8&keywords=Samsung+Galaxy+Tab+A8&nsdOptOutParam=true&pd_rd_i=B0DF2TBQBH&pd_rd_r=c73e9d03-5158-4085-82ea-d308c9252477&pd_rd_w=hXrdJ&pd_rd_wg=FblZr&pf_rd_p=cd33fa60-47cd-49f3-80b8-24b1f0cbef97&pf_rd_r=JHHH30JC20FH029APQMC&qid=1737207472&s=electronics&sbo=RZvfv%2F%2FHxDF%2BO5021pAnSA%3D%3D&sprefix=samsung+galaxy+tab+a8%2Celectronics%2C578&sr=1-5-ced4eeeb-b190-41d6-902a-1ecb3fb8b7c4-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9zZWFyY2hfdGhlbWF0aWM&psc=1",
+    description:
+      "HONOR Pad X8A with Free Flip-Cover 27.94Cm (11 Inch) Wi-Fi Tablet,4GB Ram 128 GB ROM Expandable Up to 1TB FHD Display 90Hz Refresh Rate,8300Mah Battery,Quad Speakers,Snapdragon 680,Android 14,Grey",
+    price: "10999",
+  },
+
+  {
+    id: 16,
+    category: "tablets",
+    title: "Samsung Galaxy Tab A9",
+    imgSrc: "https://m.media-amazon.com/images/I/61MTXDqdidL._SY355_.jpg",
+    amazonLink: "https://www.amazon.in/Samsung-Galaxy-Tab-A9-Expandable/dp/B0CJ37WPMK/ref=sr_1_10?crid=3GCJMO6XXQWK4&dib=eyJ2IjoiMSJ9.CyT-gugaUUk3nWx09IRpf2YWZwNnx5YEvMPGkP_-hanXpB1gx_dcinE_0nKcaW69b6OXihSwsneNcsSsAa_HYaNg4Iswx8iV4JefO3YnZrozFL0TDpWbXxW5W_Dl-abad_4_CzPJ5faH3MrnOwAPxMRm4Lc1k0CvBvcIkYfGxo8mdMlMyvtLeFdS2hrLjqvoEMhhDDkDttnBGeai8DfWjPZHavx8z_wNQbJVYG-SK_KzejHfMVVEKNv3tDl0aX9yD8AytUT5lYaHybGBOUP6JS6W2IRxP3vTR0oJbtFiXVENjrhUtJeh4UD0kzttWTnUfg7msfSeYusM-I20TpqpbwZhL3crgCy1tDTzJ1bh5nY.4nrzxstJ36kBBY8eMhINr7UJ2cB91Hqf4-alWW_YsI8&dib_tag=se&keywords=Samsung+Galaxy+Tab+A8&nsdOptOutParam=true&qid=1737207472&s=electronics&sprefix=samsung+galaxy+tab+a8%2Celectronics%2C578&sr=1-10",
+    description:
+      "Samsung Galaxy Tab A9 22.10 cm (8.7 inch) Display, RAM 4 GB, ROM 64 GB Expandable, Wi-Fi Tablet, Gray",
+    price: "11999",
+  },
+
+  {
+    id: 17,
+    category: "tablets",
+    title: "Apple iPad Air with A14 Bionic chip",
+    imgSrc: "https://m.media-amazon.com/images/I/71kFzFbiINL._SX522_.jpg",
+    amazonLink: "https://www.amazon.in/gp/aw/d/B08J6Z3TGQ/?_encoding=UTF8&pd_rd_plhdr=t&aaxitk=08b2f0ab89b47b805ed1c221aa49fa12&hsa_cr_id=4783767360102&sr=1-1-undefined&ref_=sbx_be_dp_arbies_lsi4d_asin_0_img&pd_rd_w=bu3N4&content-id=amzn1.sym.8b13584a-c7fa-402c-b0a3-ddf39b989f1f&pf_rd_p=8b13584a-c7fa-402c-b0a3-ddf39b989f1f&pf_rd_r=93236TSACG19P04H9WMV&pd_rd_wg=gK0v9&pd_rd_r=087eb355-5d1a-4a6d-bd4f-808e4976363a",
+    description:
+      "Apple 2020 iPad Air with A14 Bionic chip (10.9-inch/27.69 cm, Wi-Fi + Cellular, 64GB) - Sky Blue (4th Generation)",
+    price: "66999",
+  },
+
+  {
+    id: 18,
+    category: "laptops",
+    title: "Macbook M2",
+    imgSrc: "https://m.media-amazon.com/images/I/71r8h9mLzHL._SX466_.jpg",
+    amazonLink: "https://www.amazon.in/2022-Apple-MacBook-Laptop-chip/dp/B0DLHGQ17K/ref=sr_1_2_sspa?crid=28HMENG7D73KW&dib=eyJ2IjoiMSJ9.3T8fjFyvGONWBTVI0Dpqdl4PyRvlcwLvFvVLPGIvAtocQYTEkBpS_Ho8GaQzaJZOrIoCutg6ndqa0zJj9bAYyd-XEe-GbCL9MklFCulr-JrA0fUrwhQdoaFqVEXNZOq4e_xg2myZdKq6Gc-gYsm8uZ-GWeBijHDrguUUHGoJh71kmn9yJfVD9QT5HaeE_Bw0yM0P5pWDUKViB7wo66XaQA4OQX5cazA0tZESs9ew0nQ.UuDp97WkdhwIIaOuXOCYv2QuYYk2hapWynnjKRoGelM&dib_tag=se&keywords=macbook+m1&qid=1737224223&sprefix=mac%2Caps%2C282&sr=8-2-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1",
+    description: "Apple MacBook Air Laptop with M2 chip: 13.6-inch Liquid Retina Display, 16GB RAM, 256GB SSD Storage, Backlit Keyboard, 1080p FaceTime HD Camera. Works with iPhone and iPad; Space GrayMacbook M1 13 inch 8GB RAM 256GB SSD",
+    price: "94999",
+  },
+
+  {
+    id: 19,
+    category: "laptops",
+    title: "Macbook M3",
+    imgSrc: "https://m.media-amazon.com/images/I/71x9JI-Il0L._SX466_.jpg",
+    amazonLink: "https://www.amazon.in/Apple-MacBook-13-inch-8%E2%80%91core-Unified/dp/B0DLHYCZMV/ref=sr_1_1_sspa?crid=DNQWDY74FVXG&dib=eyJ2IjoiMSJ9.75TtB9WI0Csul_8gRRgbOQXb5JhufdZ7ut3ifXdznEDn8MXYn6q66Ey4xjURqQ7q9b1UaSEcZIkASgZXo9GstHE6E-kjT7CvxTcSbKzdfDT-xSQBs4YoWmQVjTCIvnCVtl1CzMNc2DngxvqjGdPu9JINfy4VEzD4S7FvZDt6ith-qCwpJIXW90Tl1gRce1J2Dcwm3F3qjplaprOarOIlMrOiGMmosCACmwbvcc66hSk.vAU8-SJaUTfGuONqJBbujDiAxAJYLMkM2ySRHTYGOOU&dib_tag=se&keywords=macbook+m3+air&qid=1737225518&sprefix=macbook+m%2Caps%2C306&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1",
+    description: "Apple MacBook Air (13-inch, Apple M3 chip with 8‑core CPU and 8‑core GPU, 16GB Unified Memory, 256GB) - Midnight",
+    price: "109999",
+  },
+
+  {
+    id: 20,
+    category: "laptops",
+    title: "Macbook M4 Pro",
+    imgSrc: "https://m.media-amazon.com/images/I/61eA9PkZ07L._SX466_.jpg",
+    amazonLink: "https://www.amazon.in/Apple-2024-MacBook-Laptop-10%E2%80%91core/dp/B0DLHXVCXJ/ref=sr_1_1_sspa?crid=1LRNH0SZUHHU&dib=eyJ2IjoiMSJ9.eCn5a-F33b-FtIqzfHbZVw-mX6NKw03oUD2fMXpFHJOn8xGVGJyCmxtMElcEKtP2uRZXVzZjadyq3F-GsqqYZGtEILwJ2TbznNKZBxKMo4KO0wd71Wt4GZsUYAgcliOcQNV9Uus9KYQsz9gSbkonHsh8KDoWwy1Chkdnj4MnGqEE_6Ra1WTvyesS3u1Grl-p3GpDz8GG109rrGFbAilnamfo8IvDZxCAuVvJ2tNSbHw.S5fqUeFp3wQXkeXm65CupdNhBiVFetxf7y5aVyvo-g8&dib_tag=se&keywords=macbook+m4+air&qid=1737226022&sprefix=macbook+m4+air%2Caps%2C302&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1",
+    description: "Apple MacBook Pro Laptop with M4 chip with 10‑core CPU and 10‑core GPU: Built for Apple Intelligence, (14.2″) Liquid Retina XDR Display, 16GB Unified Memory, 512GB SSD Storage; Space Black",
+    price: "109999",
+  },
+
+  {
+    id: 21,
+    category: "laptops",
+    title: "MSI Katana 17",
+    imgSrc: "https://m.media-amazon.com/images/I/71eT72ZWtKL._SY450_.jpg",
+    amazonLink: "https://www.amazon.in/MSI-i7-13700H-Windows-GeForce-B13VGK-1231IN/dp/B0CXDXNKCN/ref=sr_1_1_sspa?crid=3CRW4VVPCUDUL&dib=eyJ2IjoiMSJ9.2txpo9pNSxFCM2301OiqFnrQoJoe7NLvY7XcEOCUdijCIMLpLXYa06E0kMKLDkpkBnbJFh3XIlY06ShFVogyC1CRM6bl122XPjArpT2wNbPX1OMGyZYFVhw0sEnkHg6_2n8dt__wgXjkg-Ic4nYIkD2Iq_NWSMo85ChGgXkTVV-_c6md7l9IZ6c25j46xaz2LYMKuuKw6b4--BR_j8IWQ4cIwkYjipyLwJn6WxclHvE.mFsCu9ljcuyM4NHsu_irVc9AfIY0W4MxXkMqJdF43PE&dib_tag=se&keywords=msi+katana+17&nsdOptOutParam=true&qid=1737226224&sprefix=msi%2Caps%2C1149&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1",
+    description: "MSI Katana 17, Intel Core i7-13700H, 44CM QHD 240Hz Gaming Laptop(16GB/1TB NVMe SSD/Windows 11 Home/NVIDIA GeForce RTX 4070, GDDR 8GB/Black/2.6Kg), B13VGK-1231IN",
+    price: "124999",
+  },
+
+  {
+    id: 22,
+    category: "laptops",
+    title: "MSI Katana 17",
+    imgSrc: "https://m.media-amazon.com/images/I/71+bSm87KmL._SY450_.jpg",
+    amazonLink: "https://www.amazon.in/MSI-i7-13620H-Windows-GeForce-B13UDXK-255IN/dp/B0BTW1RCWZ/ref=sr_1_3?crid=3CRW4VVPCUDUL&dib=eyJ2IjoiMSJ9.2txpo9pNSxFCM2301OiqFnrQoJoe7NLvY7XcEOCUdijCIMLpLXYa06E0kMKLDkpkBnbJFh3XIlY06ShFVogyC1CRM6bl122XPjArpT2wNbPX1OMGyZYFVhw0sEnkHg6_2n8dt__wgXjkg-Ic4nYIkD2Iq_NWSMo85ChGgXkTVV-_c6md7l9IZ6c25j46xaz2LYMKuuKw6b4--BR_j8IWQ4cIwkYjipyLwJn6WxclHvE.mFsCu9ljcuyM4NHsu_irVc9AfIY0W4MxXkMqJdF43PE&dib_tag=se&keywords=msi+katana+17&nsdOptOutParam=true&qid=1737226224&sprefix=msi%2Caps%2C1149&sr=8-3",
+    description: "MSI Katana 17, Intel 13th Gen. i7-13620H, 44CM FHD 144Hz Gaming Laptop (16GB/512GB NVMe SSD/Windows 11 Home/Nvidia GeForce RTX3050, 6GB GDDR6/Black/2.6Kg), B13UDXK-255IN",
+    price: "76999",
+  },
+
+  {
+    id: 23,
+    category: "laptops",
+    title: "ASUS TUF Gaming A15",
+    imgSrc: "https://m.media-amazon.com/images/I/61pfnWFMZEL._SY450_.jpg",
+    amazonLink: "https://www.amazon.in/ASUS-15-6-inch-GeForce-Windows-FA507NUR-LP082W/dp/B0D5DCSNM1/ref=sxin_15_pa_sp_search_thematic_sspa?content-id=amzn1.sym.cd33fa60-47cd-49f3-80b8-24b1f0cbef97%3Aamzn1.sym.cd33fa60-47cd-49f3-80b8-24b1f0cbef97&crid=3CRW4VVPCUDUL&cv_ct_cx=msi+katana+17&keywords=msi+katana+17&nsdOptOutParam=true&pd_rd_i=B0D5DCSNM1&pd_rd_r=abd8b113-b7c8-4e4f-a687-6eba358753d8&pd_rd_w=v2a4K&pd_rd_wg=W9bwa&pf_rd_p=cd33fa60-47cd-49f3-80b8-24b1f0cbef97&pf_rd_r=GTHBJS7XZHM0N9DQV10W&qid=1737226224&sbo=RZvfv%2F%2FHxDF%2BO5021pAnSA%3D%3D&sprefix=msi%2Caps%2C1149&sr=1-3-ced4eeeb-b190-41d6-902a-1ecb3fb8b7c4-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9zZWFyY2hfdGhlbWF0aWM&psc=1",
+    description: "ASUS TUF Gaming A15, AMD Ryzen 7 7435HS, 15.6-inch FHD 144Hz, Gaming Laptop (16GB/512GB SSD/NVIDIA GeForce RTX 4050/Windows 11/90WHr/Mecha Gray/2.2 Kg), FA507NUR-LP082W",
+    price: "89999",
+  },
+
+  {
+    id: 24,
+    category: "laptops",
+    title: "Acer ALG Gaming Laptop",
+    imgSrc: "https://m.media-amazon.com/images/I/815ZrCdBiqL._SY450_.jpg",
+    amazonLink: "https://www.amazon.in/Acer-Processor-Graphics-Win11Home-AL15G-53/dp/B0DFHFCS4Y/ref=sr_1_14?crid=3CRW4VVPCUDUL&dib=eyJ2IjoiMSJ9.2txpo9pNSxFCM2301OiqFnrQoJoe7NLvY7XcEOCUdijCIMLpLXYa06E0kMKLDkpkBnbJFh3XIlY06ShFVogyC1CRM6bl122XPjArpT2wNbPX1OMGyZYFVhw0sEnkHg6_2n8dt__wgXjkg-Ic4nYIkD2Iq_NWSMo85ChGgXkTVV-_c6md7l9IZ6c25j46xaz2LYMKuuKw6b4--BR_j8IWQ4cIwkYjipyLwJn6WxclHvE.mFsCu9ljcuyM4NHsu_irVc9AfIY0W4MxXkMqJdF43PE&dib_tag=se&keywords=msi+katana+17&nsdOptOutParam=true&qid=1737226224&sprefix=msi%2Caps%2C1149&sr=8-14",
+    description: "Acer ALG Gaming Laptop 13th Gen Intel Core i7 Processor (16GB DDR4/512GB SSD/6GB RTX3050 Graphics/144Hz/Win11Home/Wifi 6) AL15G-53, 39.62cm (15.6 inch) FHD Display, Premium Metal Body, Steel Gray, 1.99KG",
+    price: "67999",
+  },
+
+  {
+    id: 25,
+    category: "laptops",
+    title: "HP OMEN Gaming AI Laptop",
+    imgSrc: "https://m.media-amazon.com/images/I/71S1eXJmIAL._SY450_.jpg",
+    amazonLink: "https://www.amazon.in/Choice-HP-Gaming-Response-xd0020AX/dp/B0CCVZP835/ref=sr_1_20?crid=3CRW4VVPCUDUL&dib=eyJ2IjoiMSJ9.2txpo9pNSxFCM2301OiqFnrQoJoe7NLvY7XcEOCUdijCIMLpLXYa06E0kMKLDkpkBnbJFh3XIlY06ShFVogyC1CRM6bl122XPjArpT2wNbPX1OMGyZYFVhw0sEnkHg6_2n8dt__wgXjkg-Ic4nYIkD2Iq_NWSMo85ChGgXkTVV-_c6md7l9IZ6c25j46xaz2LYMKuuKw6b4--BR_j8IWQ4cIwkYjipyLwJn6WxclHvE.mFsCu9ljcuyM4NHsu_irVc9AfIY0W4MxXkMqJdF43PE&dib_tag=se&keywords=msi+katana+17&nsdOptOutParam=true&qid=1737226224&sprefix=msi%2Caps%2C1149&sr=8-20",
+    description: "HP OMEN Gaming AI Laptop, AMD Ryzen 7 7840HS, 8GB NVIDIA RTX 4060, 16GB DDR5, 1TB SSD, (Win 11, Office 21, Black, 2.37kg), 165Hz, 7MS, IPS, 16.1-inch (40.9cm), FHD, Tempest Cooling, RGB KB, xd0020AX",
+    price: "109999",
+  },
+
 ];
