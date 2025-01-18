f06049a8d81c50414d180b2ce91bd44d123f4f14
farhan745 <farhanzawad187@gmail.com>
2025-01-18 18:04:04 +0600

    Project React

---
+++ E-Cart/src/components/Data.jsx
@@ -0,0 +1,99 @@
+export const items = [
+  {
+    id: 1,
+    category: "mobiles",
+    title: "Apple iPhone 14",
+    imgSrc: "https://m.media-amazon.com/images/I/71xb2xkN5qL._SL1500_.jpg",
+    amazonLink: "https://amzn.to/3PuckZp",
+    description: "Apple iPhone 14 (128 GB) - Blue",
+    price: "89999",
+  },
+  {
+    id: 10,
+    category: "Android",
+    title: "Samsung Galaxy S24 Ultra 5G",
+    imgSrc:
+      "https://brotherselectronicsbd.com/image/cache/catalog/demo/product/Samsung/S24%20Ultra/Samsung-Galaxy-S24-ultra-Titanium-Gray-800x800.jpg",
+    amazonLink:
+      "https://www.amazon.in/Samsung-Galaxy-Ultra-Green-Storage/dp/B0BT9CXXXX/ref=sr_1_1_sspa?dib=eyJ2IjoiMSJ9.O3xB_kiiFUsvf5zmsm5j4g8vzEK17lmDvw9dtn0_XEsGhZ-yQGnazJ796J6Z9Q7qN6tZCwLFzXJj0yj_UHkeAEY8KYp8I0qKTcQN8jWWP_vCy8j1Cw1aAWSeOsbVAs-eMZqb5XqhhWpo_ZR5SMNETCYxysAE5hWuBkLaR6ZoKgkyOYF57xNmdVChovkjqygyqdJUxeDcGkQdtJtnOmln6yhVX9A3ZHDlQe0PbgotGfo.PzUO-omJZuGcZEZonh960HIAaG_JkQaxDG4TEt37Bak&dib_tag=se&keywords=S24&qid=1737128302&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1",
+    description: "Samsung Galaxy S24 Ultra 5G (Green, 12GB RAM, 256GB Storage)",
+    price: "80999",
+  },
+  
+  {
+    id: 2,
+    category: "mobiles",
+    title: "Apple iPhone 15",
+    imgSrc: "https://adminapi.applegadgetsbd.com/storage/media/large/iPhone-15-Plus-(6)-2055.jpg",
+    description: "Apple iPhone 13 (64GB) - Blue",
+    price: "79999",
+  },
+   {
+    id: 3,
+    category: "mobiles",
+    title: "Apple iPhone 13",
+    imgSrc: "https://m.media-amazon.com/images/I/71xb2xkN5qL._SL1500_.jpg",
+    amazonLink: "https://amzn.to/3r6XJtv",
+    description: "Apple iPhone 13 (128GB) - Blue",
+    price: "69999",
+  },
+  {
+    id: 5,
+    category: "laptops",
+    title: "Macbook M1",
+    imgSrc: "https://m.media-amazon.com/images/I/71TPda7cwUL._SL1500_.jpg",
+    amazonLink: "https://amzn.to/3EzeQaC",
+    description: "Macbook M1 13 inch 8GB RAM 256GB SSD",
+    price: "49999",
+  },
+  {
+    id: 7,
+    category: "tablets",
+    title: "Xiaomi Pad 6",
+    imgSrc: "https://brotherselectronicsbd.com/image/cache/catalog/demo/Accessories/Xiaomi/Pad6%20Key/pad%206%20keyboard-600x600.jpg",
+    amazonLink: "https://amzn.to/3ZeNRdY",
+    description: "Xiaomi Pad 6| Qualcomm Snapdragon 870| ",
+    price: "29999",
+  },
+  
+  {
+    id: 6,
+    category: "tablets",
+    title: "Lenovo Tab P12 Pro AMOLED",
+    imgSrc: "https://m.media-amazon.com/images/I/51b9LjzmPCL._SL1080_.jpg",
+    amazonLink: "https://amzn.to/3RabsdH",
+    description:
+      "Lenovo Tab P12 Pro AMOLED (12.6 inch (32 cm), 8 GB, 256 GB, Wi-fi Only), Storm Grey with Precision Pen 3",
+    price: "19999",
+  },
+ 
+  {
+    id: 4,
+    category: "laptops",
+    title: "Lenovo IdeaPad Slim 5 ",
+    imgSrc: "https://m.media-amazon.com/images/I/71TPda7cwUL._SL1500_.jpg",
+    amazonLink: "https://amzn.to/44JsK4r",
+    description: 'Lenovo IdeaPad Slim 5 Intel Core i5 12450H 14" (35.5cm) ',
+    price: "29999",
+  },
+  {
+    id: 9,
+    category: "tablets",
+    title: "Samsung Galaxy Tab A8",
+    imgSrc: "https://m.media-amazon.com/images/I/51b9LjzmPCL._SL1080_.jpg",
+    amazonLink: "https://amzn.to/3Pdsh4F",
+    description:
+      "Samsung Galaxy Tab A8 26.69cm (10.5 inch) Display, RAM 4 GB, ROM 64 GB Expandable, Wi-Fi Tablet, Gray, (SM-X200NZAEINU)",
+    price: "49999",
+  },
+  {
+    id: 8,
+    category: "laptops",
+    title: "Apple MacBook Air Laptop",
+    imgSrc: "https://m.media-amazon.com/images/I/71TPda7cwUL._SL1500_.jpg",
+    amazonLink: "https://amzn.to/3r9rvhm",
+    description:
+      "Apple MacBook Air Laptop M1 chip, 13.3-inch/33.74 cm Retina Display, ",
+    price: "89999",
+  },
+];
